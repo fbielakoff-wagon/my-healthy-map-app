28866f4dbe55bf86c78c4ab36371e60be65b07c0
Ruth Brennan ruth.b.brennan@gmail.com <ruth.b.brennan@gmail.com>
2026-07-13 19:23:36 +0100

    Create core models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  has_many :spots, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :favourites, dependent: :destroy
+  has_many :shares, dependent: :destroy
+
+  has_one :preference, dependent: :destroy
+
+  has_many :active_follows,
+           class_name: "Follow",
+           foreign_key: :follower_id,
+           dependent: :destroy
+
+  has_many :following,
+           through: :active_follows,
+           source: :following
+
+  has_many :passive_follows,
+           class_name: "Follow",
+           foreign_key: :following_id,
+           dependent: :destroy
+
+  has_many :followers,
+           through: :passive_follows,
+           source: :follower
 end
