1ff4d7b09cd4e7896af5569a533f3c1547668630
Ruth Brennan ruth.b.brennan@gmail.com <ruth.b.brennan@gmail.com>
2026-07-15 15:01:32 +0100

    Implement favourites and preferences

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
   def edit
+    @preference = current_user.preference || current_user.build_preference
   end
 
   def update
